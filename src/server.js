47ca244c32de08e5041739bf6e59730ceefbe3af
luarrekcah <raulrodriguesdemoraes@gmail.com>
2023-02-12 18:06:29 -0500

    [ended]

--- src/server.js
+++ src/server.js
@@ -3,6 +3,7 @@ const createError = require('http-errors');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const helmet = require('helmet');
+const cors = require('cors');
 
 const indexRouter = require('./routes/index');
 
@@ -11,6 +12,7 @@ const errorHandler = require('./middleware/errorHandler');
 const app = express();
 
 app.use(helmet());
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json({limit: '50mb'}));
 app.use(express.urlencoded({limit: '50mb', extended: true }));
@@ -18,8 +20,9 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
 app.use((req,res,next)=>{
-  const host = req.get('host');
-  if(host === 'localhost:3000') {
+  const host = req.get('origin');
+  console.log(host);
+  if(host === 'https://www.dlwalt.com') { //put your URL
     next();
   } else {
     return res.sendStatus(401)
